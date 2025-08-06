4992229c920c041606db7470f604d64cf909ce8b
Amirmehdig <amirmahdig138@gmail.com>
2025-08-06 13:16:17 +0330

    feat: Add consumer test

--- consumer/src/main/java/ir/sobhan/internship/consumer/utils/NumberConsumerRoute.java
+++ consumer/src/main/java/ir/sobhan/internship/consumer/utils/NumberConsumerRoute.java
@@ -1,6 +1,8 @@
 package ir.sobhan.internship.consumer.utils;
 
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 import org.apache.camel.builder.RouteBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -9,26 +11,37 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class NumberConsumerRoute extends RouteBuilder {
 
+    @Setter
     @Value("${camel.component.kafka.brokers:localhost:9092}")
     private String kafkaBroker;
 
+    @Setter
     @Value("${camel.default.route-id:default-route-id}")
     private String routeId;
 
+    @Setter
     @Value("${consumer.interval-in-sec}")
     private int interval;
 
-    private final NumberAggregationStrategy numberAggregationStrategy;
-    private final SumNumbersProcess sumNumbersProcess;
+    @Getter
+    @Setter
+    private int sum = 0;
 
     @Override
     public void configure() {
-        String uri = "kafka:random-numbers?brokers=" + kafkaBroker;
-        from(uri)
+        from(kafkaBroker)
                 .routeId(routeId)
-                .aggregate(constant(true), numberAggregationStrategy)
-                .log("Received numbers: ${body}")
-                .completionInterval((long) interval * 1000)
-                .process(sumNumbersProcess);
+                .process(exchange -> {
+                    String body = exchange.getIn().getBody(String.class).trim();
+                    int newNumber = Integer.parseInt(body);
+                    sum += newNumber;
+                    log.info("Received {}", newNumber);
+                });
+
+        from("timer:aggregateAdder?period=" + (interval * 1000))
+                .process(exchange -> {
+                    log.info("Sum of recent numbers is {}", sum);
+                    sum = 0;
+                });
     }
 }
--- consumer/src/test/java/ir/sobhan/internship/consumer/utils/NumberConsumerRouteTest.java
+++ consumer/src/test/java/ir/sobhan/internship/consumer/utils/NumberConsumerRouteTest.java
@@ -2,47 +2,56 @@ package ir.sobhan.internship.consumer.utils;
 
 import org.apache.camel.CamelContext;
 import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
 import org.apache.camel.builder.ExchangeBuilder;
 import org.apache.camel.impl.DefaultCamelContext;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
-import java.util.List;
+class NumberConsumerRouteTest {
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-public class NumberConsumerRouteTest {
-
-    private NumberAggregationStrategy aggregationStrategy;
-    private SumNumbersProcess process;
-
-    private CamelContext context;
+    private NumberConsumerRoute numberConsumerRoute;
+    private CamelContext camelContext;
+    private Processor kafkaMessageProcessor;
 
     @BeforeEach
-    void setUp() {
-        context = new DefaultCamelContext();
-        aggregationStrategy = new NumberAggregationStrategy();
-        process = new SumNumbersProcess();
+    void setUp() throws Exception {
+        numberConsumerRoute = new NumberConsumerRoute();
+        numberConsumerRoute.setSum(0);
+        camelContext = new DefaultCamelContext();
+
+        numberConsumerRoute.setKafkaBroker("direct:start");
+        numberConsumerRoute.setRouteId("test-rout");
+
+        camelContext.addRoutes(numberConsumerRoute);
+        camelContext.start();
+        kafkaMessageProcessor = camelContext.getRoute("test-rout").getProcessor();
     }
 
     @Test
-    void aggregateAndSumTest() {
-        Exchange ex1 = ExchangeBuilder.anExchange(context)
-                .withBody(10)
+    void testProcessKafkaMessage() throws Exception {
+        numberConsumerRoute.setRouteId("test-route");
+
+        Exchange exchange1 = ExchangeBuilder.anExchange(camelContext)
+                .withBody("5")
                 .build();
+        kafkaMessageProcessor.process(exchange1);
 
-        Exchange ex2 = ExchangeBuilder.anExchange(context)
-                .withBody(15)
+        assertEquals(5, numberConsumerRoute.getSum());
+
+        Exchange exchange2 = ExchangeBuilder.anExchange(camelContext)
+                .withBody("10")
                 .build();
+        kafkaMessageProcessor.process(exchange2);
 
-        Exchange aggregated = aggregationStrategy.aggregate(null, ex1);
-        aggregated = aggregationStrategy.aggregate(aggregated, ex2);
+        assertEquals(15, numberConsumerRoute.getSum());
 
-        List<Integer> resultList = aggregated.getMessage().getBody(List.class);
-        assertEquals(2, resultList.size());
-        assertEquals(10, resultList.get(0));
-        assertEquals(15, resultList.get(1));
+        Exchange exchange3 = ExchangeBuilder.anExchange(camelContext)
+                .withBody("90")
+                .build();
+        kafkaMessageProcessor.process(exchange3);
 
-        process.process(aggregated);
+        assertEquals(105, numberConsumerRoute.getSum());
     }
 }
